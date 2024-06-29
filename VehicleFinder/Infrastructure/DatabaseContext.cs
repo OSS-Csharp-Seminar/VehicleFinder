e9030f065bfc4c0fe1f2766db4635f8b7260299b
Luka <LPolic@its.jnj.com>
2024-06-29 12:04:46 +0200

    Added migration for inserting user and admin into db.

--- VehicleFinder/Infrastructure/DatabaseContext.cs
+++ VehicleFinder/Infrastructure/DatabaseContext.cs
@@ -18,6 +18,8 @@ namespace VehicleFinder.Infrastructure
 
             ConfigureRoles(modelBuilder);
 
+            SeedUsers(modelBuilder);
+
             modelBuilder.UseSerialColumns();
 
             modelBuilder.Entity<Listing>()
@@ -44,15 +46,73 @@ namespace VehicleFinder.Infrastructure
         public DbSet<Engine> Engines { get; set; }
         public DbSet<Body> Bodies { get; set; }
 
+        public IdentityRole userRole;
+        public IdentityRole adminRole;
+
         private void ConfigureRoles(ModelBuilder modelBuilder)
         {
-            var userRole = new IdentityRole("USER");
+            userRole = new IdentityRole("USER");
             userRole.NormalizedName = "USER";
 
-            var adminRole = new IdentityRole("ADMIN");
+            adminRole = new IdentityRole("ADMIN");
             adminRole.NormalizedName = "ADMIN";
 
             modelBuilder.Entity<IdentityRole>().HasData(userRole, adminRole);
+        }
+
+        private void SeedUsers(ModelBuilder modelBuilder)
+        {
+            var hasher = new PasswordHasher<User>();
+
+            // User with 'USER' role
+            var normalUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "user@user",
+                NormalizedUserName = "USER@USER",
+                Email = "user@user",
+                NormalizedEmail = "USER@USER",
+                EmailConfirmed = false,
+                PhoneNumber = "1234567890",
+                PhoneNumberConfirmed = true,
+                FirstName = "user",
+                LastName = "user",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            normalUser.PasswordHash = hasher.HashPassword(normalUser, "aA!123123");
+
+            // User with 'ADMIN' role
+            var adminUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "admin@eadmin",
+                NormalizedUserName = "ADMIN@ADMIN",
+                Email = "admin@eadmin",
+                NormalizedEmail = "ADMIN@ADMIN",
+                EmailConfirmed = false,
+                PhoneNumber = "1234567890",
+                PhoneNumberConfirmed = true,
+                FirstName = "admin",
+                LastName = "admin",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            adminUser.PasswordHash = hasher.HashPassword(adminUser, "aA!123123");
+
+            modelBuilder.Entity<User>().HasData(normalUser, adminUser);
+
+            // Assign roles
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRole.Id,
+                    UserId = normalUser.Id
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRole.Id,
+                    UserId = adminUser.Id
+                }
+            );
         }
     }
 
---
+++ VehicleFinder/Migrations/20240629095031_latest4.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VehicleFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class latest4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "01b0a53a-4845-4d5a-9403-f9b759a580b0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c9ff91fa-f4da-4a53-af40-1b3f802b9abd");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7ec1bda5-19e9-4e1c-8e31-bdc93371099c", null, "ADMIN", "ADMIN" },
+                    { "f19649d9-88a2-4c70-b61e-c60cce7b6166", null, "USER", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7ec1bda5-19e9-4e1c-8e31-bdc93371099c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f19649d9-88a2-4c70-b61e-c60cce7b6166");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "01b0a53a-4845-4d5a-9403-f9b759a580b0", null, "USER", "USER" },
+                    { "c9ff91fa-f4da-4a53-af40-1b3f802b9abd", null, "ADMIN", "ADMIN" }
+                });
+        }
+    }
+}
