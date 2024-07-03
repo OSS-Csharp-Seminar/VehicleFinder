6fab4e5a874718c6ccc9c8a7e3cdb0eb9dd7472e
MarinoLucicRaguz <marinolucicraguz@gmail.com>
2024-07-03 18:14:56 +0200

    Implemented delete of listing

--- VehicleFinder/Areas/Listing/Pages/ListingDetails.cshtml.cs
+++ VehicleFinder/Areas/Listing/Pages/ListingDetails.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VehicleFinder.DTOs;
+using VehicleFinder.Entities;
 using VehicleFinder.Services;
 
 namespace VehicleFinder.Areas.Listing.Pages
@@ -8,16 +10,20 @@ namespace VehicleFinder.Areas.Listing.Pages
     public class ListingDetailsModel : PageModel
     {
         private readonly IListingService _listingService;
+        private readonly UserManager<User> _userManager;
 
-        public ListingDetailsModel(IListingService listingService)
+        public ListingDetailsModel(IListingService listingService, UserManager<User> userManager)
         {
             _listingService = listingService;
+            _userManager = userManager;
         }
 
         public GetGeneralListingDTO GeneralListing { get; set; }
+        public string CurrentUserId { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
+            CurrentUserId = _userManager.GetUserId(User);
             GeneralListing = await _listingService.GetGeneralListingByIdAsync(id);
 
             if (GeneralListing == null)
